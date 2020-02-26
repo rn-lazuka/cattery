c42bdac2c9992444f45f765d68dd278c2b03971f
Roman Lazuka <rn.lazuka@gmail.com>
2020-02-27 00:06:31 +0300

    Updates

---
+++ precache-manifest.a75dc607b6908d1b43072a4ba8fdb9b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afcb803c2e5cb5e4969c33d1f7806d4",
+    "url": "/cattery/index.html"
+  },
+  {
+    "revision": "43c979ddb1833ebb4fec",
+    "url": "/cattery/static/css/main.756f0ea0.chunk.css"
+  },
+  {
+    "revision": "aa196227efdeea2c6349",
+    "url": "/cattery/static/js/2.8578d862.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cattery/static/js/2.8578d862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c979ddb1833ebb4fec",
+    "url": "/cattery/static/js/main.99f9bfcf.chunk.js"
+  },
+  {
+    "revision": "04b4588cf865191fc326",
+    "url": "/cattery/static/js/runtime-main.84d800c6.js"
+  },
+  {
+    "revision": "9dfeca75e1d48d2284a57611ef8963f9",
+    "url": "/cattery/static/media/fon.9dfeca75.jpg"
+  },
+  {
+    "revision": "dec3ae469dd213185ed580f4223f0396",
+    "url": "/cattery/static/media/loading.dec3ae46.svg"
+  }
+]);
